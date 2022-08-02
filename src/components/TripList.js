65d3e63fcd6701cdd4ca39bad332b68899ba93e2
Yehonatan Shechter <ydshechter@gmail.com>
2022-08-02 16:21:31 +0300

    fetching data with useEffect (plus CSS)

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,17 +1,35 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+
+// style
+import './TripList.css'
 
 
 const TripList = () => {
 
     const [trips, setTrips] = useState([])
-    console.log(trips)
-    fetch('http://localhost:3000/trips') // fetch data from db.json
-        .then(response => response.json()) // returns json data
-        .then(json => setTrips(json)) // when there is data - update the state
 
+    useEffect(() => {
+
+        fetch('http://localhost:3000/trips') // fetch data from db.json
+            .then(response => response.json()) // returns json data
+            .then(json => setTrips(json)) // when there is data - update the state
+
+    }, []) // empty dependancy arry makes sure the function only runs once
+
+    console.log(trips)
 
     return (
-        <h2>Trip List</h2>
+        <div className="trip-list">
+            <h2>Trip List</h2>
+            <ul>
+                {trips.map(trip => (
+                    <li key={trip.id}>
+                        <h3>{trip.title}</h3>
+                        <p>{trip.price}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
